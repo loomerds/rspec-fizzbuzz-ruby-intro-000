66b9b6fd7d6d1179998ab4975fe55dac6fa41c35
Doug Loomer <loomerds@gmail.com>
2019-05-23 17:27:06 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz num
-  puts num
-  if num / 3 == 0
+  if num % 3 == 0
     return "Fizz"
-  elsif num / 5 == 0
+  elsif num % 5 == 0
     return "Buzz"
-  elsif num /15 == 0
+  elsif num % 15 == 0
     return "FizzBuzz"
   else
     return nil
